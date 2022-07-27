7c9d888aa5e3eb27b1d7d67a4ea1cfba8ef33b73
keremtpl <keremtpl57@gmail.com>
2022-07-27 16:34:31 +0300

    github button designed and added this project

---
+++ Unity-tab-system/Assets/SocialMedia&rate&feedback buttons/ButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void goToGitHub()
+    {
+        Application.OpenURL("https://github.com/ShadoutGames");
+    }
+}
